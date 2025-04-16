8716c167a2fcc740390686e9f9585c3ee52526df
BernardoBahia <bernardobebahia@gmail.com>
2025-04-16 18:39:51 -0300

    feat(backend): Adiciona entidade Event

---
+++ backend/src/domain/event/enterprise/entities/event.entity.ts
@@ -0,0 +1,77 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+export type EventType = 'ENTRY' | 'EXIT'
+
+export interface EventProps {
+  managerId: UniqueEntityID
+  driverId: UniqueEntityID
+  cardId: UniqueEntityID
+  odometer: number
+  status: EventType
+  startAt: Date
+  endAt?: Date
+  createdAt: Date
+  updatedAt?: Date
+}
+
+export class Event extends Entity<EventProps> {
+  static create(props: Omit<EventProps, 'createdAt'>, id?: UniqueEntityID) {
+    const now = new Date()
+    const event = new Event(
+      {
+        ...props,
+        createdAt: now,
+      },
+      id,
+    )
+
+    return event
+  }
+
+  get managerId() {
+    return this.props.managerId
+  }
+
+  get driverId() {
+    return this.props.driverId
+  }
+
+  get cardId() {
+    return this.props.cardId
+  }
+
+  get odometer() {
+    return this.props.odometer
+  }
+
+  get status() {
+    return this.props.status
+  }
+
+  get startAt() {
+    return this.props.startAt
+  }
+
+  get endAt() {
+    return this.props.endAt
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  finalize(endAt: Date, odometer: number) {
+    this.props.endAt = endAt
+    this.props.odometer = odometer
+    this.touch()
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+}
