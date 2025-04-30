60be650ad7171bea368ea8e22e4b8515893ce4c9
BernardoBahia <bernardobebahia@gmail.com>
2025-04-30 16:31:47 -0300

    feat(backend): Adiciona DriverRepository

---
+++ backend/src/domain/driver/application/repositories/driver-repository.ts
@@ -0,0 +1,11 @@
+import { Driver } from '../../enterprise/entities/driver.entity'
+
+export abstract class DriverRepository {
+  abstract findById(id: string): Promise<Driver | null>
+  abstract findByEmail(email: string): Promise<Driver | null>
+  abstract findByCNH(cnh: string): Promise<Driver | null>
+  abstract create(driver: Driver): Promise<Driver>
+  abstract save(driver: Driver): Promise<Driver>
+  abstract delete(id: string): Promise<void>
+  abstract findAll(): Promise<Driver[]>
+}
