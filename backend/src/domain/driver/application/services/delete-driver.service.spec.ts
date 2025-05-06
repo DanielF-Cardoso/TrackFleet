9f96348beee94bb29e511678b4fbf5bcf3dc978f
BernardoBahia <bernardobebahia@gmail.com>
2025-05-06 18:03:04 -0300

    feat(backend): Adiciona DeleteDriverService e seus testes

---
+++ backend/src/domain/driver/application/services/delete-driver.service.spec.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest'
+import { InMemoryDriverRepository } from 'test/repositories/in-memory-driver.repository'
+import { ResourceNotFoundError } from './errors/resource-not-found.error'
+import { I18nService } from 'nestjs-i18n'
+import { FakeLogger } from 'test/fake/logs-mocks'
+import { makeDriver } from 'test/factories/driver/make-driver'
+import { DeleteDriverService } from './delete-driver.service'
+
+let sut: DeleteDriverService
+let driverRepository: InMemoryDriverRepository
+let i18n: I18nService
+let logger: FakeLogger
+
+beforeEach(() => {
+  driverRepository = new InMemoryDriverRepository()
+
+  i18n = {
+    translate: vi.fn(),
+  } as unknown as I18nService
+
+  logger = new FakeLogger()
+
+  sut = new DeleteDriverService(driverRepository, i18n, logger)
+})
+
+describe('DeleteDriverService', () => {
+  it('should remove a driver', async () => {
+    const driver = makeDriver()
+    await driverRepository.create(driver)
+
+    const result = await sut.execute({ driverId: driver.id.toString() })
+
+    expect(result.isRight()).toBe(true)
+
+    expect(driverRepository.items).toHaveLength(0)
+  })
+
+  it('should return error with translated message if driver is not found', async () => {
+    vi.spyOn(i18n, 'translate').mockResolvedValue('Driver not found.')
+
+    const result = await sut.execute({ driverId: 'non-existent-id' })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(ResourceNotFoundError)
+    if (result.value instanceof ResourceNotFoundError) {
+      expect(result.value.message).toBe('Driver not found.')
+    }
+  })
+})
---
+++ backend/src/domain/driver/application/services/delete-driver.service.ts
@@ -0,0 +1,50 @@
+import { Inject, Injectable, LoggerService } from '@nestjs/common'
+import { DriverRepository } from '../repositories/driver-repository'
+import { Either, left, right } from '@/core/errors/either'
+import { I18nService } from 'nestjs-i18n'
+import { LOGGER_SERVICE } from '@/infra/logger/logger.module'
+import { ResourceNotFoundError } from './errors/resource-not-found.error'
+
+interface DeleteDriverServiceRequest {
+  driverId: string
+}
+
+type DeleteDriverServiceResponse = Either<ResourceNotFoundError, null>
+
+@Injectable()
+export class DeleteDriverService {
+  constructor(
+    private driverRepository: DriverRepository,
+    private i18n: I18nService,
+    @Inject(LOGGER_SERVICE)
+    private readonly logger: LoggerService,
+  ) {}
+
+  async execute({
+    driverId,
+  }: DeleteDriverServiceRequest): Promise<DeleteDriverServiceResponse> {
+    this.logger.log(
+      `Fetching driver for delete: ${driverId}`,
+      'DeleteDriverServiceService',
+    )
+
+    const driver = await this.driverRepository.findById(driverId)
+
+    if (!driver) {
+      const errorMessage = await this.i18n.translate('errors.driver.notFound')
+      this.logger.warn(
+        `Driver not found for driverId: ${driverId}`,
+        'DeleteDriverServiceService',
+      )
+      return left(new ResourceNotFoundError(errorMessage))
+    }
+
+    await this.driverRepository.delete(driverId)
+
+    this.logger.log(
+      `Driver with ID ${driverId} deleted successfully`,
+      'DeleteDriverServiceService',
+    )
+    return right(null)
+  }
+}
