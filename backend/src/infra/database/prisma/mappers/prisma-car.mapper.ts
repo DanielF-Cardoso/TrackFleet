dcaed2a36a1f71c86d5253e5a9236a63b2e84f74
Bernardo Bahia <bernardobebahia@gmail.com>
2025-05-12 18:13:19 -0300

    feat(backend): Adiciona PrismaCarRepository

---
+++ backend/src/infra/database/prisma/mappers/prisma-car.mapper.ts
@@ -0,0 +1,44 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { LicensePlate } from '@/core/value-objects/license-plate.vo'
+import { Renavam } from '@/core/value-objects/renavam.vo'
+import { Car } from '@/domain/cars/enterprise/entities/car.entity'
+import { Cars as PrismaCar, Prisma } from '@prisma/client'
+
+export class PrismaCarMapper {
+  static toDomain(raw: PrismaCar): Car {
+    const car = Car.create(
+      {
+        managerId: new UniqueEntityID(raw.managerId),
+        licensePlate: new LicensePlate(raw.licensePlate),
+        brand: raw.brand,
+        model: raw.model,
+        year: raw.year,
+        color: raw.color,
+        odometer: raw.odometer,
+        status: raw.status as 'AVAILABLE' | 'IN_USE' | 'IN_MAINTENANCE',
+        renavam: new Renavam(raw.renavam),
+        updatedAt: raw.updatedAt ?? undefined,
+      },
+      new UniqueEntityID(raw.id),
+    )
+
+    return car
+  }
+
+  static toPersistence(car: Car): Prisma.CarsUncheckedCreateInput {
+    return {
+      id: car.id.toString(),
+      managerId: car.managerId.toString(),
+      licensePlate: car.licensePlate.toValue(),
+      brand: car.brand,
+      model: car.model,
+      year: car.year,
+      color: car.color,
+      odometer: car.odometer,
+      status: car.status,
+      renavam: car.renavam.toValue(),
+      createdAt: car.createdAt,
+      updatedAt: car.updatedAt ?? undefined,
+    }
+  }
+}
---
+++ backend/src/infra/database/prisma/repositories/prisma-car.repository.ts
@@ -0,0 +1,79 @@
+import { Injectable } from '@nestjs/common'
+import { CarRepository } from '@/domain/cars/application/repositories/car-repository'
+import { Car } from '@/domain/cars/enterprise/entities/car.entity'
+import { LicensePlate } from '@/core/value-objects/license-plate.vo'
+import { PrismaService } from '../prisma.service'
+import { Renavam } from '@/core/value-objects/renavam.vo'
+import { PrismaCarMapper } from '../mappers/prisma-car.mapper'
+
+@Injectable()
+export class PrismaCarRepository implements CarRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async create(car: Car): Promise<Car> {
+    const data = PrismaCarMapper.toPersistence(car)
+
+    const createdCar = await this.prisma.cars.create({ data })
+
+    return PrismaCarMapper.toDomain(createdCar)
+  }
+
+  async findByLicensePlate(
+    licensePlate: string | LicensePlate,
+  ): Promise<Car | null> {
+    const value =
+      typeof licensePlate === 'string' ? licensePlate : licensePlate.toValue()
+
+    const car = await this.prisma.cars.findUnique({
+      where: { licensePlate: value },
+    })
+
+    if (!car) return null
+
+    return PrismaCarMapper.toDomain(car)
+  }
+
+  async findByRenavam(renavam: string | Renavam): Promise<Car | null> {
+    const value = typeof renavam === 'string' ? renavam : renavam.toValue()
+
+    const car = await this.prisma.cars.findUnique({
+      where: { renavam: value },
+    })
+
+    if (!car) return null
+
+    return PrismaCarMapper.toDomain(car)
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.prisma.cars.delete({
+      where: { id },
+    })
+  }
+
+  async findById(id: string): Promise<Car | null> {
+    const car = await this.prisma.cars.findUnique({
+      where: { id },
+    })
+
+    if (!car) return null
+
+    return PrismaCarMapper.toDomain(car)
+  }
+
+  async save(car: Car): Promise<Car> {
+    const data = PrismaCarMapper.toPersistence(car)
+
+    const updatedCar = await this.prisma.cars.update({
+      where: { id: data.id },
+      data,
+    })
+
+    return PrismaCarMapper.toDomain(updatedCar)
+  }
+
+  async findAll(): Promise<Car[]> {
+    const cars = await this.prisma.cars.findMany()
+    return cars.map(PrismaCarMapper.toDomain)
+  }
+}
